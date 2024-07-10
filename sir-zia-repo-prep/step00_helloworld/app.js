e9b83e42e30394fbced8f00e14b118411159381c
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-07-10 14:13:46 +0500

    yes

--- sir-zia-repo-prep/step00_helloworld/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let message = "Hello World!, This is Nabeel Ali";
-console.log(message);
--- sir-zia-repo-prep/step00_helloworld/app.ts
+++
@@ -1,2 +0,0 @@
-let message:string = "Hello World!, This is Nabeel Ali";
-console.log(message);
